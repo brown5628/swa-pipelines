f2355b166ca724f87361d410d9d8d3e5492bdb66
Will Brown <brown5628@gmail.com>
2022-01-09 09:31:44 -0500

    schedule testing

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,4 @@
-from dagster import job, op, fs_io_manager
+from dagster import job, op, fs_io_manager, repository, schedule, ScheduleDefinition
 from dagster.utils.log import get_dagster_logger
 
 logger = get_dagster_logger()
@@ -7,13 +7,29 @@ logger = get_dagster_logger()
 def get_name():
     return "dagster"
 
+@op
+def get_date():
+    return "today"
 
 @op
-def hello(name: str):
+def hello(name:str, date:str):
     logger.info("It worked!")
-    print(f"Hello, {name}! This is dagster.")
+    print(f"Hello, {name}! This is dagster on {date}.")
 
 
 @job
 def hello_dagster():
-    hello(get_name())
+    hello(get_name(),get_date())
+
+@schedule(
+    cron_schedule="*/15 * * * *",
+    job=hello_dagster,
+    execution_timezone="US/Central",
+)
+def test_schedule(context):
+    date = context.scheduled_execution_time.strftime("%Y-%m-%d")
+    return {"ops": {"hello": {"config": {"date": date}}}}
+
+@repository
+def swa_elt_repository():
+    return [hello_dagster, test_schedule]
--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 cp dagster.yaml /tmp/
-dagit -h 0.0.0.0 -p 8080
+dagster-daemon run & dagit -h 0.0.0.0 -p 8080
